35ce2d5e77a27c76786635346966bf95d0dc92f3
Caleb C <wolfertamer@gmail.com>
2024-10-15 12:38:33 -0700

    added more logic for logging in

--- scripts/loginHandler.js
+++ scripts/loginHandler.js
@@ -1,3 +1,10 @@
+chrome.storage.local.get(["loggedin"]).then((result) => {
+    if(result && !window.location.href.includes('popup_alt.html')) {
+        window.location.href = 'popup_alt.html'
+    } else if(window.location.href.includes('popup.html')) {
+        window.location.href = 'popup.html'
+    }
+});
 
 //Event DOMContentLoaded runs when everything is fully loaded.
 document.addEventListener('DOMContentLoaded', function() {
@@ -5,8 +12,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById("loginForm")
 
     //When the submit button is pressed, do something.
+    if(!form) {
+        return
+    }
     form.addEventListener("submit", (event) => {
-        let username = document.getElementById("username")
+        event.preventDefault()
+
+        let usernameTag = document.getElementById("username")
+        let passwordTag = document.getElementById("password")
+
+        let username = usernameTag.value
+        let password = passwordTag.value
+
+        if(password !== '' && username !== '') {
+            usernameTag.setAttribute('disabled',true)
+            passwordTag.setAttribute('disabled',true)
+            chrome.storage.local.set({ "loggedin": true }).then(() => {
+                console.log("Value is set");
+            });
+            window.location.href = 'popup_alt.html'
+
+        } else {
+            document.getElementById('error').style.display = "initial"
+        }
         //WARNING, console.log() does not work in this section. Consider using document.write() instead.
     })
 })
